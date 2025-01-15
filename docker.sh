101c792e44895a6e8105ad97e5b32a45d7366e81
CalvinLe08 <lenguyenkhang102002@gmail.com>
2025-01-15 17:15:12 +0700

    docker.sh updated

--- docker.sh
+++ docker.sh
@@ -4,7 +4,6 @@ POSTGRES_USER=todo_user
 POSTGRES_PASSWORD=todo_password
 POSTGRES_DB=tododb
 POSTGRES_PORT=5435
-HOST_DATA_DIR=/absolute/path/to/postgres_data
 
 docker pull postgres:15-alpine
 
@@ -14,5 +13,5 @@ docker run -d \
   -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
   -e POSTGRES_DB=$POSTGRES_DB \
   -p $POSTGRES_PORT:5432 \
-  -v $HOST_DATA_DIR:/var/lib/postgresql/data \
+  -v postgres_data:/var/lib/postgresql/data \
   postgres:15-alpine
